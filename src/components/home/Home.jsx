eb9a1c48201acc67bbe723c162c42d52c626da98
Sergey Fedorov <mailcorp@mail.ru>
2023-11-26 20:55:41 +0300

    add services

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,52 +1,75 @@
-import {useEffect} from "react"
-import { Button, Col, Form, Row, Select, Switch } from "antd"
-import {optionsForHomeSelect, preparedFormItems} from "./constants"
-import {UsergroupAddOutlined} from "@ant-design/icons"
+import { useEffect, useState } from "react";
+import { Button, Col, Form, Row, Select, Switch } from "antd";
+import { optionsForHomeSelect, preparedFormItems } from "./constants";
+import { UsergroupAddOutlined } from "@ant-design/icons";
+import scheduleService from "../services/schedule"
 
-import "./style.css"
-
+import "./style.css";
 
 const Home = () => {
-  const [ form ] = Form.useForm()
+  const [schedules, setSchedules] = useState([])
+  const [webDevelop, setWebDevelop] = useState("")
+  const [dataScience, setDataScience] = useState("")
 
-    useEffect(() => {
-        form.setFieldsValue({
-            switch1: false,
-            switch2: false,
-            switch3: false,
-            switch4: false,
-            select: 'web'
-        })
-    }, []);
+  const [form] = Form.useForm();
 
-    console.log(form)
-    const getFields = async () => {
-        const values = await form.validateFields()
-        return values
+  useEffect(() => {
+    form.setFieldsValue({
+      switch1: false,
+      switch2: false,
+      switch3: false,
+      switch4: false,
+      select: "web",
+    });
+  }, []);
+
+
+  const getFields = async () => {
+    const values = await form.validateFields();
+    return {
+      ...values,
+      select: webDevelop + dataScience
+    };
+  };
+
+
+  const getFirstDateAndSecondDate = () => {
+    let currentDate = Math.floor(new Date().getTime() / 1000)
+    return {
+      firstDate: currentDate,
+      secondDate: currentDate + 5 * 24 * 60 * 60
     }
+  }
 
   return (
     <div className="home">
-      <h3>Добро пожаловать</h3>
-      <Form
-        className="form"
-        form={form}
-      >
+      <h3 className="welcome">Добро пожаловать</h3>
+      <Form className="form" form={form}>
         <h4>Выберите подгруппу по английскому языку </h4>
-        {
-          preparedFormItems.map((item) => {
-            return  <Row align={"middle"} justify={"space-between"} key={item.name}>
-              <Col span={16}>
-                <h4><UsergroupAddOutlined />{' ' + item.label}</h4>
-              </Col>
-              <Col>
-                <Form.Item name={item.name}>
-                  <Switch defaultChecked={false} />
-                </Form.Item>
-              </Col>
-            </Row>
-          })
-        }
+        <div className="switch-container">
+          {preparedFormItems.map((item) => {
+            return (
+              <Row
+                align={"middle"}
+                justify={"space-between"}
+                key={item.name}
+                className="form-item-row"
+              >
+                <Col span={16}>
+                  <h4>
+                    <UsergroupAddOutlined />
+                    {" " + item.label}
+                  </h4>
+                </Col>
+                <Col>
+                  <Form.Item name={item.name}>
+                    <Switch defaultChecked={false} />
+                  </Form.Item>
+                </Col>
+              </Row>
+            );
+          })}
+        </div>
 
         <Form.Item
           className="last-form-item"
@@ -54,19 +77,41 @@ const Home = () => {
           label={<h3 className="select-direction">Выберете направление</h3>}
           labelAlign={"left"}
         >
-          <Select options={optionsForHomeSelect} defaultValue={"web"} />
+          <Select
+            defaultOpen={true}
+            options={optionsForHomeSelect}
+            defaultValue={"web"}
+            className="select"
+          />
         </Form.Item>
 
-        <Button className="button-continue" type={"primary"} onClick={() => {
-            getFields().then(data => {
-                console.log(data)
-            })
-        }}>
-          Продолжить
-        </Button>
+   {/*     <h3 className="select-direction">Выберете направление</h3>
+        <div className="last-form-item">
+          <Button onClick={() => {
+            setDataScience('')
+            setWebDevelop("Web-develop")}}>Web-разработка</Button>
+          <Button onClick={() => {
+            setWebDevelop('')
+            setDataScience("DataScience")}}>Наука о данных</Button>
+        </div>*/}
+        <div>
+          <Button
+            className="button-continue"
+            type={"primary"}
+            onClick={async () => {
+             const data =  await scheduleService.getSchedule(getFirstDateAndSecondDate().firstDate, getFirstDateAndSecondDate().secondDate)
+              setSchedules(data)
+              //getFields().then((data) => {
+                //console.log(data);
+              //});
+            }}
+          >
+            Продолжить
+          </Button>
+        </div>
       </Form>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
